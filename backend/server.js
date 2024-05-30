d43066d602236aa1682cf367337600e59cc73860
huslenhub <101103560+huslenhub@users.noreply.github.com>
2024-05-30 13:19:21 +0900

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,24 @@ const app = express();
 const port = process.env.PORT || 5001;
 const apiKey = process.env.OPENAI_API_KEY;
 
+let selectedGender = null;
+
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.static('public'));
+//성별 받기
+app.post('/api/gender', (req, res) => {
+  const { gender } = req.body;
+  selectedGender = gender;
+  console.log('Received gender:', gender);// 없어도 됨
+  
+  res.sendStatus(200); 
+});
 
+//외모 조건 받고, 이미지 생성해서, 이미지 주소 보냄 
 app.post('/api/generate', async (req, res) => {
-  const { description } = req.body;
-
+  const { data } = req.body; 
+  const description = `${data} 를 만족하는 ${selectedGender} 한 명의 사진`;
+  console.log('Received data from client fixed:', description); // 변경된 부분
   try {
     const response = await axios.post(
       'https://api.openai.com/v1/images/generations',
@@ -34,19 +45,15 @@ app.post('/api/generate', async (req, res) => {
       }
     );
 
-  
-    const imageUrls = response.data.data.map(image => image.url);
-    res.json({ imageUrls });
-  } catch (error) {
-    console.error('Error generating image:', error.response ? error.response.data : error.message);
-    if (!res.headersSent) {
-      res.status(500).json({ error: error.response ? error.response.data : error.message });
+    const imageUrl = response.data.data[0].url;
+    res.json({ imageUrl });
+    
+  }  catch (error) {
+      console.error('Error generating image:', error.response ? error.response.data : error.message);
+      if (!res.headersSent) {
+        res.status(500).json({ error: error.response ? error.response.data : error.message });
+      }
     }
-  }
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
